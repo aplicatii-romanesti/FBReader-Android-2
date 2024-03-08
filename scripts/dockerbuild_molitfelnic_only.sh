5d1f3166299e0f20f6d90e15f5785a5c88578ece
aplicatii-romanesti <aplicatii.romanesti@gmail.com>
2024-03-08 20:05:16 +0200

    mingc vers

--- scripts/dockerbuild_molitfelnic_only.sh
+++ scripts/dockerbuild_molitfelnic_only.sh
@@ -67,7 +67,7 @@ cp ~/777/aplicatii.romanesti-release-key.keystore ~/FBReader-Android-2/
 cd ~/
 docker rm -f fb || true
 #docker run --name fb -ti -v `pwd`/FBReader-Android-2:/p mingc/android-build-box:1.11.1 bash -c 'cd /p/ && ./gradlew  --gradle-user-home=/p/.gradle/ clean assembleRelease' | tee -a $GIT_BRANCH.log
-docker run --rm --name fb -ti -v `pwd`/FBReader-Android-2:/p mingc/android-build-box:$(cat $BUILD_FOLDER/scripts/dockerBuilderImage.txt) bash -c 'cd /p/ && ./gradlew  --gradle-user-home=/p/.gradle/ assembleRelease' | tee -a $NAME.log
+docker run --rm --name fb -ti -v `pwd`/FBReader-Android-2:/p $(cat $BUILD_FOLDER/scripts/dockerBuilderImage.txt) bash -c 'cd /p/ && ./gradlew  --gradle-user-home=/p/.gradle/ assembleRelease' | tee -a $NAME.log
 # --rm
 
 #or only pack:
