00bd78e246803aaa9daeaa510faeb00194a53efa
aplicatii-romanesti <aplicatii.romanesti@gmail.com>
2025-08-19 17:37:46 +0300

    version in file name

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -57,8 +57,10 @@ cd $BUILD_FOLDER
 export GIT_BRANCH=$(git branch | grep '*' | cut -d' ' -f2)
 cd -
 
-NAME="${APP}_${GIT_BRANCH}_${DATE_START}"
-echo "FYI, GIT_BRANCH=$GIT_BRANCH at date: $DATE_START (approx: `date`)" | tee ${NAME}.log
+VERSION=$(cat fbreader/app/VERSION)
+
+NAME="${APP}_${GIT_BRANCH}_${VERSION}_${DATE_START}"
+echo "FYI, VERSION=$VERSION, GIT_BRANCH=$GIT_BRANCH at date: $DATE_START (approx: `date`)" | tee ${NAME}.log
 
 cp ~/777/aplicatii.romanesti-release-key.keystore $BUILD_FOLDER/
 cp ~/777/aplicatii.romanesti-upload-key.jks $BUILD_FOLDER/
